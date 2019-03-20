f8d653ac954311d641ad7fded63a9e7236e46eb2
samoha01 <samoha1@list.ru>
2019-03-20 14:16:24 +0300

    FinalTask_2 Done

---
+++ src/FinalTask_2.java
@@ -0,0 +1,36 @@
+/*
+   Итоговое задание № 2
+ Написать программу сортировки по возрастанию заданного пользователем массива чисел
+
+*/
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class FinalTask_2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Задайте длину массива: ");
+        int n = scan.nextInt();
+        int mas[] = new int[n];
+        for (int q = 0; q < n; q++) {
+            System.out.print("Присвойте число элементу массива [" + (q+1) +"] :");
+            mas[q] = scan.nextInt();
+        }
+        boolean isSorted = false;
+        int buf;
+        System.out.println("Массив до сортировки:" + Arrays.toString(mas));
+        while(!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < mas.length-1; i++) {
+                if(mas[i] > mas[i+1]){
+                    isSorted = false;
+                    buf = mas[i];
+                    mas[i] = mas[i+1];
+                    mas[i+1] = buf;
+                }
+            }
+        }
+        System.out.println("Отсортированный массиив" + Arrays.toString(mas));
+    }
+}
